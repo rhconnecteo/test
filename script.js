8af127430d0d40175a4227b799594290568c0f63
Herizo Radilison Ramboamiarison <herizo.ramboamiarison@connecteo.in.axian-group.com>
2026-08-19 09:22:08 +0300

    'first'

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
     var isBusy = false; // vrai pendant le traitement d'un scan (évite les doublons)
     var isRunning = false; // vrai tant que la caméra scanne activement
     var selectedMode = 'entree';
-    var API_URL = (typeof window !== 'undefined' && window.API_URL) || 'https://script.google.com/macros/s/AKfycbx5QmKP2EffNwzLXfd5nx7Ztr9Qsn0ALfQbpyZhgKRxfAbTlqmgJgKOjO9iekc-xAZR/exec';
+    var API_URL = (typeof window !== 'undefined' && window.API_URL) || 'https://script.google.com/macros/s/AKfycbyGG52hD9hdsOIeRgXJnxlr9Wm9nu-xtX3E8eBfcYlHXSaSSLHTReYq1llNzFX4j-ie/exec';
     var config = { fps: 12, qrbox: { width: 250, height: 250 } };
 
     function setMode(mode) {
